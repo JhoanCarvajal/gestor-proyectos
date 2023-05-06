8a3bd1df5144c2e5fb225009cef6695e82d9c9b6
Jhoan Carvajal <jhoans127@outlook.es>
2023-05-05 20:23:44 -0500

    feat: se creó el componente de producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      image: './assets/images/pc-dañado.png',
+      price: 5000
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      image: './assets/images/pc-dañado.png',
+      price: 6000
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      image: './assets/images/pc-dañado.png',
+      price: 7000
+    }
+  ]
 
   onLoaded(img: string){
     console.log('log desde el padre', img);
