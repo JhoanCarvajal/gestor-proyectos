58090130c0c5a1cf2a03a68ee823c6d04d63b287
Jhoan Carvajal <jhoans127@outlook.es>
2023-05-10 22:59:52 -0500

    feat: se agregó el menu de navegacion para móvil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,7 @@ import { Product } from './models/product.model';
 export class AppComponent {
   imgParent = '';
 
-  products: Product[] = [
-    {
-      id: 1,
-      name: 'Product 1',
-      image: './assets/images/pc-dañado.png',
-      price: 5000
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      image: './assets/images/pc-dañado.png',
-      price: 6000
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      image: './assets/images/pc-dañado.png',
-      price: 7000
-    }
-  ]
+
 
   onLoaded(img: string){
     console.log('log desde el padre', img);
